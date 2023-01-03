f0311a15148ddc60708abeed9243d383147f4fdd
miraekwak <alfo9490@naver.com>
2023-01-03 17:23:29 +0900

    AIW20A-20

---
+++ AIW20A-20/application.py
@@ -0,0 +1,74 @@
+from kma import KMA
+from excel_file_manager import ExcelFileManager
+
+
+class Application:
+    def __init__(self, driver_path, kma_id, kma_pass, download_path):
+        self.driver_path = driver_path
+        self.kma_id = kma_id
+        self.kma_pass = kma_pass
+        self.download_path = download_path
+
+    def run(self):
+        # 검색 조건
+        start_day = '20210101'
+        end_day = '20211231'
+        weather_type = '강풍'
+
+        # 엑셀 파일 다운로드 방식
+        data = self.file_download_method(weather_type, start_day, end_day)
+
+        # 사이트 크롤링 방식
+        # data = self.crawling_data_method(weather_type, start_day, end_day)
+
+        header = ['특보 유형', '발표 시각', '지역', '발효 시각', '해당 지역', '구분']
+
+        excel_file_manager = ExcelFileManager()
+        file_name = weather_type + '_' + start_day + '_' + end_day + '.xlsx'
+        excel_file_manager.file_write(title=weather_type, file_name=file_name, data_list=data, header=header)
+
+    def file_download_method(self, weather_type, start_day, end_day):
+        kma = KMA(driver_path=self.driver_path, download_path=self.download_path)
+
+        kma.setting()
+        kma.login_loof(kma_id=self.kma_id, kma_pass=self.kma_pass)
+
+        # 파일 다운로드
+        kma.file_download(weather_type, start_day, end_day)
+        kma.quit()
+
+        # 다운로드 받은 파일 읽기
+        file_manager = ExcelFileManager()
+        full_file_path = file_manager.file_name_read(self.download_path)
+        raw_data = file_manager.file_read(full_file_path)
+
+        # 읽은 데이터 가공
+        clean_data = kma.clean_up_exel_raw_data(raw_data, weather_type)
+
+        return clean_data
+
+    def crawling_data_method(self, weather_type, start_day, end_day):
+        kma = KMA(driver_path=self.driver_path, download_path=self.download_path)
+
+        kma.setting()
+        kma.login_loof(kma_id=self.kma_id, kma_pass=self.kma_pass)
+
+        data = kma.crawling_data(weather_type, start_day, end_day)
+        kma.quit()
+
+        return data
+
+
+def main():
+    # 개인 설정
+    chrome_driver_path = r'c:\Users\~\chromedriver.exe'
+    kma_id = 'id'
+    kma_pass = 'password'
+    download_path = r'C:\Users\~\downloads\strong-wind'
+
+    a = Application(driver_path=chrome_driver_path, kma_id=kma_id, kma_pass=kma_pass, download_path=download_path)
+    a.run()
+
+
+if __name__ == '__main__':
+    main()
---
+++ AIW20A-20/excel_file_manager.py
@@ -0,0 +1,48 @@
+import os
+
+import openpyxl
+import pandas as pd
+import win32com.client as win32
+
+
+class ExcelFileManager:
+
+    def file_write(self, title, file_name, data_list, header=[]):
+        print('Creating Excel File...')
+        excel_file = openpyxl.Workbook()
+        excel_sheet = excel_file.active
+        excel_sheet.title = title
+
+        if len(header) != 0:
+            excel_sheet.append(header)
+        for data in data_list:
+            excel_sheet.append(data)
+        excel_file.save(file_name)
+        excel_file.close()
+        print('Excel File Creation Successful')
+
+    def file_read(self, file_path):
+        df = pd.read_excel(file_path, skiprows=1, header=None, engine='openpyxl')
+        return df
+
+    def file_name_read(self, file_path):
+        # 다운로드 받은 파일 이름을 알아내 경로 반환
+        file_path_slash = self.backslash_to_slash(file_path)
+        file_name_list = os.listdir(file_path_slash)
+        # 다운로드 1개 파일 -> 1개 파일만 읽음
+        for file_name in file_name_list:
+            if file_name.split('.')[1] == 'xls':
+                path = self.xls_to_xlsx(file_path + '\\'+file_name)
+                return self.backslash_to_slash(path)
+
+    def xls_to_xlsx(self, file_path):
+        excel = win32.gencache.EnsureDispatch('Excel.Application')
+        wb = excel.Workbooks.Open(file_path)
+
+        wb.SaveAs(file_path + "x", FileFormat=51)  # FileFormat = 51 is for .xlsx extension
+        wb.Close()  # FileFormat = 56 is for .xls extension
+        excel.Application.Quit()
+        return file_path.split('.')[0] + '.xlsx'
+
+    def backslash_to_slash(self, path):
+        return path.replace('\\', '/')
