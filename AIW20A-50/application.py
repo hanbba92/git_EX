545cede909137fe6c9d6bdbcfd8a03c4794eaa31
kanghanbit <hanbba92@gmail.com>
2023-07-11 15:18:43 +0900

    Vertical windshear code update

--- AIW20A-50/application.py
+++ AIW20A-50/application.py
@@ -23,21 +23,22 @@ class Application(object):
         VGRD_1 = inp['VGRD_1'] # Comparing Level
         UGRD_1 = inp['UGRD_1']
         DZDT_1 = inp['DZDT_1']
-        Shape_info = VGRD_0.shape # array structure
-
-        #각 레벨 벡터 (x,y,z) 구현
-        Base_Level= zip(UGRD_0.flatten(),VGRD_0.flatten(),DZDT_0.flatten()) # 1-Dimension으로 바꾸어 각 좌표의 바람 성분 3가지를 하나로 묶는다.
-        Compare_Level= zip(UGRD_1.flatten(),VGRD_1.flatten(),DZDT_1.flatten())
 
 
-        #벡터 차 구하기
-        wind_shear_list = [] # 각 좌표의 wind shear 최종 리스트
-        for (Base,Compare) in zip(Base_Level,Compare_Level):
-            wind_shear=np.linalg.norm(np.array(np.array(Base)-np.array(Compare))) # Base와 Comparing level 차의 벡터 크기
-            wind_shear_list.append(wind_shear)
-        wind_shear_list=np.array(wind_shear_list)
-        Wind_Shear=wind_shear_list.reshape(Shape_info) #원래 좌표에 따른 바람 성분 파일 구조로 변환
-        task_file_manager.write(self.input_file, [inp['latitude'], inp['longitude'], Wind_Shear],
+        #각 레벨 벡터의 차 X,Y,Z
+        X = UGRD_0-UGRD_1
+        Y = VGRD_0-VGRD_1
+        Z = DZDT_0-DZDT_1
+
+        #벡터 크기 구하기 (각 레벨 벡터 차의 제곱의 합과 그 제곱급 구하기)
+
+        X = X**2
+        Y = Y**2
+        Z = Z**2
+        wind_shear = X+Y+Z
+        wind_shear = wind_shear**(1/2)
+
+        task_file_manager.write(self.input_file, [inp['latitude'], inp['longitude'], wind_shear],
                                 task_number=self.output_task)
 
 
